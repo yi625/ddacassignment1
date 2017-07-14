f06ac3e7342fc0da2c5a3f4360e41fce34c2610c
yi625 <xiongyi703@gmail.com>
2017-07-14 10:09:16 +0800

    Add files via upload

---
+++ MySession.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDAC
+{
+    public class MySession
+    {
+        // private constructor
+        private MySession()
+        {
+            AgentID = -1;
+            StaffID = -1;
+            Username = "";
+            UserEmail = "";
+            UserRole = "";
+        }
+        // Gets the current session.
+        public static MySession Current
+        {
+            get
+            {
+                MySession session =
+                  (MySession)HttpContext.Current.Session["__MySession__"];
+                if (session == null)
+                {
+                    session = new MySession();
+                    HttpContext.Current.Session["__MySession__"] = session;
+                }
+                return session;
+            }
+        }
+        public int AgentID { get; set; }
+        public int StaffID { get; set; }
+        public string Username { get; set; }
+        public string UserEmail { get; set; }
+        public string UserRole { get; set; }
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DDAC.Startup))]
+namespace DDAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
